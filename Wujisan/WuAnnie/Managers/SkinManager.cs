890a6e0d5c1f7898e3925e361e40b62eec750523
chienhao10 <huangjianhao_10@hotmail.com>
2016-02-19 14:47:26 -0500

    更新安妮
    更新安妮

---
+++ Wujisan/WuAnnie/Managers/SkinManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK.Menu;
+
+namespace WuAIO.Managers
+{
+    class SkinManager
+    {
+        public Menu menu { get; set; }
+
+        readonly AIHeroClient Player = EloBuddy.Player.Instance;
+
+        public SkinManager(int skins)
+        {
+            //Creating menu
+            menu = MenuManager.AddSubMenu("换肤功能");
+            {
+                menu.NewCheckbox("enable", "开启换肤");
+                menu.NewSlider("skinid", "皮肤ID", 0, 0, skins, true);
+            }
+
+            Game.OnTick += Game_OnTick;
+        }
+
+        private void Game_OnTick(EventArgs args)
+        {
+            if (Player.SkinId != menu.Value("skinid") && menu.IsActive("enable"))
+            {
+                Player.SetSkinId(menu.Value("skinid"));
+            }
+
+            return;
+        }
+    }
+}
---
+++ Wujisan/WuAnnie/Managers/VersionManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Net;
+using EloBuddy;
+using Version = System.Version;
+
+namespace WuAIO.Managers
+{
+    static class VersionManager
+    {
+        public static Version AssVersion { get { return Assembly.GetExecutingAssembly().GetName().Version; } }
+
+        public static void CheckVersion()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                try
+                {
+                    var champ = Player.Instance.ChampionName == "MasterYi" ? "Yi" : Player.Instance.ChampionName;
+
+                    string Text = new WebClient().DownloadString("https://raw.githubusercontent.com/WujuSan/EloBuddy/master/Wu" + champ + "/Wu" + champ + "/Properties/AssemblyInfo.cs");
+
+                    var Match = new Regex(@"\[assembly\: AssemblyVersion\(""(\d+)\.(\d+)\.(\d+)\.(\d+)""\)\]").Match(Text);
+
+                    if (Match.Success)
+                    {
+                        var CorrectVersion = new Version(string.Format("{0}.{1}.{2}.{3}", Match.Groups[1], Match.Groups[2], Match.Groups[3], Match.Groups[4]));
+
+                        if (CorrectVersion > AssVersion)
+                        {
+                            Chat.Print("<font color='#FFFF00'>你的 Wu{0}  </font><font color='#FF0000'>可更新</font><font color='#FFFF00'>, 新版本为: " + CorrectVersion + "</font>", champ);
+                            Chat.Print("<font color='#FFFF00'>你的 Wu{0}  </font><font color='#FF0000'>可更新</font><font color='#FFFF00'>, 新版本为: " + CorrectVersion + "</font>", champ);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e + "\n");
+                }
+            });
+        }
+    }
+}
